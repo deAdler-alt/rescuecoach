aa8f2665e79a4a5dbd904bc1769c155b77450efd
Kamil Piejko <173197@stud.prz.edu.pl>
2025-08-15 00:27:34 +0200

    feat(pwa): add basic Workbox runtimeCaching for HTML and assets

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,25 @@ const baseConfig: NextConfig = {
   reactStrictMode: true,
 }
 
+const runtimeCaching = [
+  // Strony (HTML) – cache'uj nawigacje
+  {
+    urlPattern: ({ request }: any) => request.mode === 'navigate',
+    handler: 'NetworkFirst',
+    options: {
+      cacheName: 'html-cache',
+      expiration: { maxEntries: 50, maxAgeSeconds: 7 * 24 * 60 * 60 },
+    },
+  },
+  // Assety (js/css/img/font)
+  {
+    urlPattern: ({ request }: any) =>
+      ['style', 'script', 'image', 'font'].includes(request.destination),
+    handler: 'StaleWhileRevalidate',
+    options: { cacheName: 'assets' },
+  },
+] as any
+
 export default withPWA({
   dest: 'public',
   register: true,
@@ -13,5 +32,6 @@ export default withPWA({
   aggressiveFrontEndNavCaching: true,
   workboxOptions: {
     maximumFileSizeToCacheInBytes: 8 * 1024 * 1024,
+    runtimeCaching,
   },
 })(baseConfig)
